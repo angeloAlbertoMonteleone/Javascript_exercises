be0c57e277d0ebb71b9dc7111bdd4dcee6a31171
Angelo Alberto Monteleone <angeloalbertomonteleone@users.noreply.github.com>
2021-01-19 20:02:10 +0100

    removing deleted files from tracking

--- modules/operations.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-/* 
-creare due moduli js e nel primo creare tutte le operazioni di una calcolatrice somma, sottrazione, divisione, moltiplicazione */
-
-export const mult = (a,b) => a * b;
-export const div = (a,b) => a / b;
-export const sum = (a,b) => a + b;
-export const sub = (a,b) => a - b;
--- modules/operationsResult.js
+++
@@ -1,7 +0,0 @@
-import { mult } from "./operations.js";
-
-"use strict";
-
-/* nel secondo modulo importare le funzioni moltiplicazione per moltiplicare due numeri e stampare a console il risultato */
-
-console.log(mult(5,10));
