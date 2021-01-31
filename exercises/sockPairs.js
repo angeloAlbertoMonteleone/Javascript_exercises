56032e1aabf29958b225e79abc86861c5142a898
Angelo Alberto Monteleone <angeloalbertomonteleone@users.noreply.github.com>
2021-01-31 20:55:49 +0100

    sock pairs exercise added

---
+++ exercises/sockPairs.js
@@ -0,0 +1,27 @@
+let arr = [10, 20, 20, 10, 10, 30, 50, 10, 20];
+
+arr.sort();
+let previous;
+const newArr = arr.reduce((acum, current)=>{
+    if (current === previous) {
+        previous = "";
+        acum += 1;
+    }else{
+        previous = current;
+    }
+    return acum;
+}, 0)
+console.log(newArr);
+
+// function sockMerchant(n, ar) {
+//     var res = 0;
+//         ar.sort();
+//         for(let i=0; i<n;i++){
+//             if(ar[i] == ar[i+1]){
+//                 i++;
+//                 res++;
+//                }
+//         }
+//     return res;
+//     }
+
