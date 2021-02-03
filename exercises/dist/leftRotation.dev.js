6eafc3ddd28c1c199f686b83eea9d437bbb0dcae
Angelo Alberto Monteleone <angeloalbertomonteleone@users.noreply.github.com>
2021-02-03 12:23:43 +0100

    solution posted

--- exercises/dist/leftRotation.dev.js
+++ exercises/dist/leftRotation.dev.js
@@ -1,7 +1,11 @@
 "use strict";
 
 var arr = [1, 2, 3, 4, 5];
+var d = 4;
 
-for (var i = 0; arr.length; i++) {
-  console.log(arr[i]);
+for (var i = 0; i <= d; i++) {
+  arr.unshift();
+  arr.shift(5);
 }
+
+console.log(arr);
