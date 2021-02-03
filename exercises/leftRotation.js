83f80cd49965da163aee1c346e46017ce4211e70
Angelo Alberto Monteleone <angeloalbertomonteleone@users.noreply.github.com>
2021-02-03 12:10:02 +0100

    solution posted

--- exercises/leftRotation.js
+++ exercises/leftRotation.js
@@ -1,5 +1,10 @@
 let arr = [1,2,3,4,5];
+let d = 4;
 
-for(let i = 0; arr.length; i++) {
-    console.log(arr[i])
+let newArr = arr.concat();
+for(let i = 0; i < d; i++) {
+    let value = newArr.shift();
+    newArr.push(value);
 }
+console.log(newArr);
+
