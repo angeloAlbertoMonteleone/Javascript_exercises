6135473ad4aa8529404f410d48c0827a0ec7c5b0
Angelo Alberto Monteleone <angeloalbertomonteleone@users.noreply.github.com>
2021-02-07 14:20:12 +0100

    second solution added

--- exercises/MathMin.js
+++ exercises/MathMin.js
@@ -4,9 +4,20 @@
 // its smallest argument. We can build something like that now. Write a function
 // min that takes two arguments and returns their minimum.
 
-const min = (arg1,arg2) => {
-    let minResult = Math.min(arg1, arg2);
-    return minResult;
+const min = (num1,num2) => {
+    let minimum = 0;
+    if(num1 < num2) {
+        return minimum += num1;
+    } if(num2 < num1) {
+        return minimum += num2;
+    } else {
+        return `the numbers are the same!`
+    }
 }
-console.log(min(5,2));
+console.log(min(7, 5))
 
+// const min = (arg1,arg2) => {
+//     let minResult = Math.min(arg1, arg2);
+//     return minResult;
+// }
+// console.log(min(5,2));
